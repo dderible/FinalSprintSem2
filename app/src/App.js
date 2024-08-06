b01ffff82bf727c0b073e0e6cf1a86e07f0869f7
dderible <164251032+dderible@users.noreply.github.com>
2024-08-06 20:53:38 +0000

    Navbar

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Navbar } from "./app/components/navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Test 2</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "App">
+        <Navbar />
     </div>
   );
 }
