708f859fd276def40680c706d9c0a5bf2d8bfa68
dderible <164251032+dderible@users.noreply.github.com>
2024-08-06 23:35:51 +0000

    removed navbar css

--- app/src/components/navbar.jsx
+++ app/src/components/navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import {ShoppingCart} from 'phosphor-react';
-import './index.css';
+import '../index.css';
 
 export const Navbar = () => {
     return ( 
