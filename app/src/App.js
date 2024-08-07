65386558147bd6bbeb54a52819ce10134972a401
dderible <164251032+dderible@users.noreply.github.com>
2024-08-07 16:58:57 +0000

    .

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navbar } from './components/navbar';
-import { Productdetails } from './pages/shop/productdetails';
-import { Shoppingcart } from './pages/cart/shoppingcart';
+import { Productdetails } from './components/pages/shop/productdetails';
+import { Shoppingcart } from './components/pages/cart/shoppingcart';
 
 function App() {
   return (
