be852287517cc214786e4290f63970d9ea39d51a
dderible <164251032+dderible@users.noreply.github.com>
2024-08-08 15:40:13 +0000

    working on api

--- app/src/App.js
+++ app/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navbar } from './components/navbar';
 import { Productdetails } from './components/pages/shop/productdetails';
 import { Shoppingcart } from './components/pages/cart/shoppingcart';
-import { ProductList } from './components/productlist'
+import { productList } from './components/productlist'
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
       <Router>
       <Navbar />
         <Routes>
-          <Route path='/' element={<ProductList />} />
+          <Route path='/' element={<productList />} />
           <Route path='/shop' element={<Productdetails />} />
           <Route path="/cart" element={<Shoppingcart />} />
         </Routes>
