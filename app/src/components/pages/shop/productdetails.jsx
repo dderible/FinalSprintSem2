01e945882e3469756166af4e66a6383e1ec8947c
Logan Oram <logan.oram@keyin.com>
2024-08-09 16:46:49 -0230

    details file

--- app/src/components/pages/shop/productdetails.jsx
+++ app/src/components/pages/shop/productdetails.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getProductsById } from '../api';
+import { useShoppingCart } from '../context/ShoppingCartContext';
+import { ShoppingCart } from 'phosphor-react';
 
-export const Productdetails = () => {
-  return (
-    <div>shop</div>
-  )
-}
+const ProductDetails = () => {
+    const { productId } = useParams();
+    const [product, setProduct] = useState(null);
+    const { addToCart } = ShoppingCart();
 
-export default Productdetails
+    useEffect(() => {
+    setProduct(getProductById(Number(productId)));
+}, [productId]);
+
+if (!product) return <div>Loading...</div>;
+
+return (
+    <div>
+        <h1>{product.name}</h1>
+        <p>{product.description}</p>
+        <p>{product.price}</p>
+        <button onClick={() => addToCart(product)}>Add To Cart</button>
+    </div>
+    );
+};
+
+export default ProductDetails;
